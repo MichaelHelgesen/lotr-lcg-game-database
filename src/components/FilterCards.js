5f87368bdd586bd62caf907c39ab041aab53beb9
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-04-07 12:38:01 +0200

    Added click handler and imported props and helpers

--- src/components/FilterCards.js
+++ src/components/FilterCards.js
@@ -1,6 +1,8 @@
 // /src/MyCustomString.js
 
 import React, { useState, useEffect } from 'react'
+import { FormField } from '@sanity/base/components'
+import PatchEvent, { set, unset } from '@sanity/form-builder/PatchEvent'
 
 
 import sanityClient from 'part:@sanity/base/client'
@@ -13,8 +15,21 @@ const query = '*[_type == "card"]'
 import { TextInput, Stack, Label, Grid, Card, Text, Flex, Box } from '@sanity/ui'
 
 export const FilterCards = React.forwardRef((props, ref) => {
+    const {
+        type,
+        value,
+        readOnly,
+        placeholder,
+        markers,
+        presence,
+        compareValue,
+        onFocus,
+        onBlur,
+        onChange,
+    } = props
 
-    const [cardList, setCardList] = useState([]);
+    const [cardList, setCardList] = useState([])
+    const [chosenCards, chooseCard] = useState([]);
 
     useEffect(() => {
         let isSubscribed = true;
@@ -28,19 +43,59 @@ export const FilterCards = React.forwardRef((props, ref) => {
             })
         return () => isSubscribed = false
     }, [])
-    return (
-        <Grid columns={[1, 2, 2, 2]} gap={[1, 1, 2, 3]} padding={0}>
-            <Card>Deck</Card>
-            <Card>
-                <Card>Cardlist</Card>
-                <Card>
-                    {
-                        cardList.length ? cardList.map((card, index) => <p key={index}>{card}</p>) : null
-                    }
-                </Card>
-            </Card>
-        </Grid>
 
+    const handleClick = React.useCallback(
+        // useCallback will help with performance
+        (event) => {
+            const inputValue = [{
+                _key: "d4b87cde6398",
+                _ref: "01001",
+                _type: "reference"
+            }]
+            // if the value exists, set the data, if not, unset the data
+            onChange(PatchEvent.from(inputValue ? set(inputValue) : unset()))
+        },
+        [onChange] // Verdien å se etter for oppdatering
+    )
+
+    const clearReferences = React.useCallback(
+        // useCallback will help with performance
+        (event) => {
+            const inputValue = null
+            // if the value exists, set the data, if not, unset the data
+            onChange(PatchEvent.from(inputValue ? set(inputValue) : unset()))
+        },
+        [onChange] // Verdien å se etter for oppdatering
+    )
+
+    return (
+        <FormField
+            description={type.description}
+            title={type.title}
+            __unstable_markers={markers}
+            __unstable_presence={presence}
+            compareValue={compareValue}
+        >
+            <Grid columns={[1, 2, 2, 2]} gap={[1, 1, 2, 3]} padding={0}>
+                <Card>Deck
+                <Card>
+                    <ul>
+                        {value.map(card => card._ref)}
+                    </ul>
+                    <p onClick={clearReferences}>Clear references</p>
+                </Card>
+                </Card>
+                
+                <Card>
+                    <Card>Cardlist</Card>
+                    <Card>
+                        {
+                            cardList.length ? cardList.map((card, index) => <Card onClick={handleClick} padding={[3, 3, 4]} radius={2} shadow={1} key={index}>{card}</Card>) : null
+                        }
+                    </Card>
+                </Card>
+            </Grid>
+        </FormField>
     )
 }
 )
