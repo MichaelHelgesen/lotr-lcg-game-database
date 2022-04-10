74a032ca426bb3c4720699c17b698f5f39635e61
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-04-10 21:56:12 +0200

    Finished adding / deleting cards and sorting

--- src/components/FilterCards.js
+++ src/components/FilterCards.js
@@ -62,9 +62,21 @@ export const FilterCards = React.forwardRef((props, ref) => {
                             }
                         })
                     }
-                    if(numberOfExistingCards) {
+                    if (numberOfExistingCards) {
                         setDeck(prevState => {
-                            return [...prevState, {...card, selected: numberOfExistingCards}]
+                            return [...prevState, { ...card, selected: numberOfExistingCards }].sort(function(a, b) {
+                                let navnA = a.name.toUpperCase(); // Ignorere store og små bokstaver
+                                let navnB = b.name.toUpperCase(); // Ignorere store og små bokstaver
+                                if (navnA < navnB) {
+                                  return -1;
+                                }
+                                if (navnA > navnB) {
+                                  return 1;
+                                }
+                              
+                                // Hvis navn er like
+                                return 0;
+                              })
                         })
                     }
                     return { ...card, selected: numberOfExistingCards }
@@ -74,22 +86,112 @@ export const FilterCards = React.forwardRef((props, ref) => {
         })
     }, []);
 
+    useEffect(() => {
+        setCardList(prevState => {
+            return prevState.map(card => {
+                let numberOfExistingCards = 0
+                if (value) {
+                    value.forEach(obj => {
+                        if (obj._ref === card._id) {
+                            numberOfExistingCards++
+                        }
+                    })
+                }
+                return { ...card, selected: numberOfExistingCards }
+            })
+        }
+        )
+
+    }, [value]);
+
     const handleClick = React.useCallback(
         (num, card) => {
-            setDeck(prevState => {
-                return [...prevState.filter(existingCard => existingCard._id != card._id), { ...card, selected: num }]
-            })
-            const action = insert(
-                [{ _ref: card._id, _type: "reference" }],
-                "after",
-                [-1]
-            );
-            onChange(PatchEvent.from(action).prepend(setIfMissing([])));
 
+            console.log(card.selected)
+            console.log(num)
+
+            if (num > card.selected && num > 0) {
+                console.log("higher number")
+                const action = insert(
+                    [{ _ref: card._id, _type: "reference" }],
+                    "after",
+                    [-1]
+                );
+                for (let i = card.selected; i < num; i++) {
+                    onChange(PatchEvent.from(action).prepend(setIfMissing([])));
+                }
+                setDeck(prevState => {
+                    return [...prevState.filter(existingCard => existingCard._id != card._id), { ...card, selected: num }].sort(function(a, b) {
+                        let navnA = a.name.toUpperCase(); // Ignorere store og små bokstaver
+                        let navnB = b.name.toUpperCase(); // Ignorere store og små bokstaver
+                        if (navnA < navnB) {
+                          return -1;
+                        }
+                        if (navnA > navnB) {
+                          return 1;
+                        }
+                      
+                        // Hvis navn er like
+                        return 0;
+                      })
+                })
+
+            } else if (num < card.selected && num > 0) {
+                console.log("lower number")
+                //console.log(props.parent.cardSelection.map(obj => obj._ref).indexOf(card._id))
+
+                for (let i = card.selected; i > num; i--) {
+                    let index = props.parent.cardSelection.map(obj => obj._ref).indexOf(card._id)
+                    client
+                        .patch(props.parent._id)
+                        .unset([`cardSelection[${index}]`])
+                        .commit()
+                }
+
+                setDeck(prevState => {
+                    return [...prevState.filter(existingCard => existingCard._id != card._id), { ...card, selected: num }].sort(function(a, b) {
+                        let navnA = a.name.toUpperCase(); // Ignorere store og små bokstaver
+                        let navnB = b.name.toUpperCase(); // Ignorere store og små bokstaver
+                        if (navnA < navnB) {
+                          return -1;
+                        }
+                        if (navnA > navnB) {
+                          return 1;
+                        }
+                      
+                        // Hvis navn er like
+                        return 0;
+                      })
+                })
+
+            } else if (num === 0 && card.selected) {
+                console.log("zero")
+                client
+                    .patch(props.parent._id)
+                    .unset([`cardSelection[_ref == "${card._id}"]`])
+                    .commit()
+                setDeck((prevState) => {
+                    return [...prevState.filter(existingCard => {
+                        return (existingCard._id != card._id)
+                    })].sort(function(a, b) {
+                        let navnA = a.name.toUpperCase(); // Ignorere store og små bokstaver
+                        let navnB = b.name.toUpperCase(); // Ignorere store og små bokstaver
+                        if (navnA < navnB) {
+                          return -1;
+                        }
+                        if (navnA > navnB) {
+                          return 1;
+                        }
+                      
+                        // Hvis navn er like
+                        return 0;
+                      })
+                });
+                //onChange(PatchEvent.from(num ? set(inputValue) : unset()));
+            }
         },
         [onChange]
     );
-    console.log("deck", deck)
 
     const clearReferences = React.useCallback(
         // useCallback will help with performance
@@ -104,31 +206,45 @@ export const FilterCards = React.forwardRef((props, ref) => {
 
     const createQuantity = (num, card) => {
 
-        let quantityArray = []
-        for (let i = 0; i < num; i++) {
-            quantityArray.push(
-                <span onClick={(event) => { handleClick(i + 1, card) }}>{i + 1}</span>
-            )
+        let quantityArray = [<span onClick={(event) => { handleClick(0, card) }} key={0}>0</span>]
+
+        for (let i = 1; i <= num; i++) {
+            if (card.selected && card.selected === i) {
+                quantityArray.push(
+                    <span style={{ background: "green" }} key={i}>{i}</span>
+                )
+            } else {
+                quantityArray.push(
+                    <span key={i} onClick={(event) => { handleClick(i, card) }}>{i}</span>
+                )
+            }
         }
         return quantityArray
     }
 
 
     return (
-        <div>
+        <FormField
+            description={type.description}
+            title={type.title}
+            __unstable_markers={markers}
+            __unstable_presence={presence}
+            compareValue={compareValue}
+            //ref={ref}
+        >
             <p onClick={clearReferences}>Clear references</p>
             Deck:
             <ul>
-                {deck.map((card, index) => <li key={index}>{card.name} - Quantity added to deck: {card.selected} / {createQuantity(card.quantity, card)}</li>)}
+                {deck.map((card, index) => <li ref={ref} onFocus={onFocus} onBlur={onBlur} key={index}>{card.name} - Quantity to add: {createQuantity(card.quantity, card)} / {card.selected}</li>)}
             </ul>
             Carpool:
             <ul>
                 {cardList.map((card, index) =>
                     <li key={index}>
-                        {card.name} - Quantity to add: {createQuantity(card.quantity, card) } / {card.selected}
+                        {card.name} - Quantity to add: {createQuantity(card.quantity, card)} / {card.selected}
                     </li>)}
             </ul>
-        </div>
+        </FormField>
     )
 })
 
