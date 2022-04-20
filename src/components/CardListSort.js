5f1483f2a1e879fa34d7871c85f9cba3eb5dfb48
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-04-20 22:31:29 +0200

    Changes

--- src/components/CardListSort.js
+++ src/components/CardListSort.js
@@ -17,65 +17,73 @@ import {
 export const CardListSort = React.forwardRef((props, ref) => {
   const { cardList, setCardList, value } = props;
   const [order, setOrder] = useState(true);
+  const [sortParameter, setSortParameter] = useState("")
+
+  const references = value.map(reference => reference._ref)
 
   const sortList = (sortParam) => {
+    console.log(sortParameter)
+    if(!sortParameter) {
+      setSortParameter(sortParam)
+      //setOrder(true)
+    } else if (sortParameter != sortParam) {
+      setSortParameter(sortParam)
+      setOrder(true)
+    } else (
+      setOrder(prevState => !prevState)
+    )
     setCardList((prevState) => {
-      return [...prevState].sort((a, b) => {
+      const newArr = prevState.map(prevObj => {
+        return {
+          ...prevObj,
+          numberInDeck: references.filter(reference => reference === prevObj._id).length
+        }
+      })
+      return [...newArr].sort((a, b) => {
         let nameA;
         let nameB;
         if (typeof a[sortParam] == "object") {
           nameA = a[sortParam]._ref.toLowerCase();
           nameB = b[sortParam]._ref.toLowerCase();
+        } else if (sortParam == "numberInDeck") {
+          nameA = a[sortParam] > 0 ? a[sortParam] : undefined
+          nameB = b[sortParam] > 0 ? b[sortParam] : undefined
         } else if (sortParam == "cost") {
           a[sortParam] ? a[sortParam] != "X" ? nameA = Number(a.cost) || 0 : nameA = 100 : nameA = a.threat;
           b[sortParam] ? b[sortParam] != "X" ? nameB = Number(b.cost) || 0 : nameB = 100 : nameB = b.threat;
-        } else if (sortParam == "defense") {
-          nameA = a[sortParam];
-          nameB = b[sortParam];
-        } else if (sortParam == "attack") {
-          nameA = a[sortParam];
-          nameB = b[sortParam];
-        } else if (sortParam == "health") {
-          nameA = a[sortParam];
-          nameB = b[sortParam];
-        } else if (sortParam == "willpower") {
-          nameA = a[sortParam];
-          nameB = b[sortParam];
         } else if (typeof a[sortParam] == "string") {
-                nameA = a[sortParam].toLowerCase()
-                nameB = b[sortParam].toLowerCase()
-           /* } else if (typeof a[sortParam] == "number") {
-                nameA = a[sortParam]
-                nameB = b[sortParam]*/
+          nameA = a[sortParam].toLowerCase()
+          nameB = b[sortParam].toLowerCase()
+        } else {
+          nameA = a[sortParam];
+          nameB = b[sortParam];
         }
-        /* if (typeof nameA != "number" && nameA === nameB) {
+        if (typeof nameA != "number" && typeof nameA != "string" && nameA === nameB) {
           return 0;
-        } else if (typeof nameA != "number") {
+        } else if (typeof nameA != "number" && typeof nameA != "string") {
           return 1;
-        } else if (typeof nameB != "number") {
+        } else if (typeof nameB != "number" && typeof nameA != "string") {
           return -1;
-        } else */ if (nameA < nameB) {
+        } else if (nameA < nameB) {
           return order ? -1 : 1;
         } else if (nameA > nameB) {
           return order ? 1 : -1;
         } else {
           return 0;
         }
-        /* if (nameA < nameB) {
-                return order ? -1 : 1
-            }
-            if (nameA > nameB) {
-                return order ? 1 : -1
-            } */
-        // return 0;
       });
     });
-    setOrder((prevState) => !prevState);
+    
   };
 
   return (
     <Flex>
-      <Box flex="1">Qty</Box>
+      <Box flex="1" onClick={() => {
+        sortList("numberInDeck");
+      }}
+      >
+        Qty
+      </Box>
       <Box
         flex="3"
         onClick={() => {
