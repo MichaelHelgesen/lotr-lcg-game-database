9260785b1a671fc1d5ee39d06f48a95c9345b85a
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-05-02 15:35:46 +0200

    adding charts

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,27 +4,32 @@ import { Line, Bar } from "react-chartjs-2";
 
 
 
-const DynamicChart = () => {
-  return (
+const DynamicChart = (props) => {
+const { deckList, value } = props
+
+
+
+const spheres = [...new Set(deckList.map(card => card.sphere._ref))].sort()
+console.log([...spheres.map(sphere => deckList.filter(ref => ref.sphere._ref == sphere && ref.cardType._ref != "hero").map(obj => obj.name + obj.quantity)), 0])
+const numberOfSpheres = [...spheres.map(sphere => deckList.filter(ref => ref.sphere._ref == sphere).map(card => card.quantity).reduce((prev, next) => prev + next, 0)), 0]
+const costBySphere = [...spheres.map(sphere => value.filter(val => val._ref == sphere && ref.cost).length), 0]
+
+const costValues = deckList.filter(card => card.cost).map(card => card.cost).sort()
+const numberOfCostValues = [0, ...new Set(costValues)].map(cost => costValues.filter(num => num == cost).length)
+
+
+
+return (
     <div className="App">
       <Line
         datasetIdKey="id"
         data={{
-          labels: ["0", "1", "2", "3"],
+          labels: [0, ...new Set(costValues)],
           datasets: [
             {
               id: 1,
-              label: "Cost",
-              data: [3, 5, 4, 2, 0],
-              /* backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
-              ], */
+              label: "Spheres",
+              data: numberOfCostValues,
               borderColor: [
                 'rgb(255, 99, 132)',
                 'rgb(255, 159, 64)',
@@ -43,12 +48,44 @@ const DynamicChart = () => {
       <Bar
         datasetIdKey="id1"
         data={{
-          labels: ["Tac", "Spi", "Neu", "Lea", "Lor"],
+          labels: spheres,
           datasets: [
             {
               id: 1,
               label: "Spheres",
-              data: [3, 5, 4, 2, 2, 0],
+              data: numberOfSpheres,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+              ],
+              borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+      />
+      <Bar
+        datasetIdKey="id2"
+        data={{
+          labels: spheres,
+          datasets: [
+            {
+              id: 1,
+              label: "Spheres",
+              data: costBySphere,
               backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(255, 159, 64, 0.2)',
